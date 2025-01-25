d4f66c91ed6d25ffde487f8586b6a2cff8b96325
Archie Jaskowicz <archiejaskowicz@gmail.com>
2025-01-25 13:25:51 +0000

    feat: initial commit

---
+++ Source/SoftwareOC/Public/SoftwareOCSettings.h
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "SoftwareOCSettings.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Config=SoftwareOC, DefaultConfig, meta=(DisplayName="Software Occlusion Culling"))
+class SOFTWAREOC_API USoftwareOCSettings : public UDeveloperSettings
+{
+	GENERATED_BODY()
+	
+public: /* Variables */
+
+	/**
+	 * @brief If this is off, you will default back to GPU Occlusion culling.
+	 */
+	UPROPERTY(Config, EditAnywhere, BlueprintReadOnly, Category="Software OC")
+	bool bEnableOcclusion;
+};
