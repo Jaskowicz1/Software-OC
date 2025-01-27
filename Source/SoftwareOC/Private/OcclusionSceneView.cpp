3fb22e24cd5bb1a4ec9499e3a6d7d8ef3f68d61c
Archie Jaskowicz <archiejaskowicz@gmail.com>
2025-01-27 08:34:42 +0000

    feat: added SceneView extension

---
+++ Source/SoftwareOC/Private/OcclusionSceneView.cpp
@@ -0,0 +1,74 @@
+﻿
+
+#include "OcclusionSceneView.h"
+
+#include "SoftwareOCSettings.h"
+#include "Runtime/Renderer/Private/ScenePrivate.h"
+#include "Runtime/Renderer/Private/SceneRendering.h"
+#include "PrimitiveSceneInfo.h"
+#include "Unreal/SceneSoftwareOcclusion.h"
+
+static FSceneSoftwareOcclusion* SceneSoftwareOcclusion;
+
+FOcclusionSceneViewExtension::FOcclusionSceneViewExtension(const FAutoRegister& AutoRegister)
+	: FSceneViewExtensionBase(AutoRegister)
+{
+	SceneSoftwareOcclusion = new FSceneSoftwareOcclusion{};
+}
+
+void FOcclusionSceneViewExtension::PostRenderBasePassDeferred_RenderThread(FRDGBuilder& GraphBuilder,
+	FSceneView& InView, const FRenderTargetBindingSlots& RenderTargets,
+	TRDGUniformBufferRef<FSceneTextureUniformParameters> SceneTextures)
+{
+	FSceneViewExtensionBase::
+		PostRenderBasePassDeferred_RenderThread(GraphBuilder, InView, RenderTargets, SceneTextures);
+
+	FSceneViewExtensionBase::PostRenderView_RenderThread(GraphBuilder, InView);
+	
+	const USoftwareOCSettings* OcclusionSettings = GetDefault<USoftwareOCSettings>();
+
+	if (!OcclusionSettings->bEnableOcclusion)
+	{
+		return;
+	}
+
+	if (!InView.bIsViewInfo)
+	{
+		return;
+	}
+
+	FViewInfo* View = (FViewInfo*)(&InView);
+	
+	if (!View || !InView.ViewActor || !InView.ViewActor->GetWorld() || !InView.ViewActor->GetWorld()->Scene)
+	{
+		return;
+	}
+
+	FScene* Scene = InView.ViewActor->GetWorld()->Scene->GetRenderScene();
+	
+	if (!Scene)
+	{
+		return;
+	}
+	
+	FSceneViewState* ViewState = (FSceneViewState*)View->State;
+
+	if (!ViewState || ViewState->bIsFrozen)
+	{
+		return;
+	}
+		
+	SceneSoftwareOcclusion->Process(Scene, *View);
+	
+	TArray<FPrimitiveSceneInfo*> AddedSceneInfos;
+	for (TConstDualSetBitIterator<SceneRenderingBitArrayAllocator, FDefaultBitArrayAllocator> BitIt(View->PrimitiveVisibilityMap, Scene->PrimitivesNeedingStaticMeshUpdate); BitIt; ++BitIt)
+	{
+		int32 PrimitiveIndex = BitIt.GetIndex();
+		AddedSceneInfos.Add(Scene->Primitives[PrimitiveIndex]);
+	}
+
+	if (AddedSceneInfos.Num() > 0)
+	{
+		FPrimitiveSceneInfo::UpdateStaticMeshes(Scene, AddedSceneInfos, EUpdateStaticMeshFlags::AllCommands);
+	}
+}
---
+++ Source/SoftwareOC/Public/OcclusionSceneView.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+#include "SceneViewExtension.h"
+
+class SOFTWAREOC_API FOcclusionSceneViewExtension : public FSceneViewExtensionBase
+{
+public:
+	FOcclusionSceneViewExtension(const FAutoRegister& AutoRegister);
+
+	/** Scene View extension interface. */
+public:
+	virtual void SetupViewFamily(FSceneViewFamily& InViewFamily) override {};
+	virtual void SetupView(FSceneViewFamily& InViewFamily, FSceneView& InView) override {};
+	virtual void BeginRenderViewFamily(FSceneViewFamily& InViewFamily) override {};
+	virtual void PreRenderViewFamily_RenderThread(FRDGBuilder& GraphBuilder, FSceneViewFamily& InViewFamily) override {};
+	virtual void PreRenderView_RenderThread(FRDGBuilder& GraphBuilder, FSceneView& InView) override {};
+	virtual void PostRenderBasePassDeferred_RenderThread(FRDGBuilder& GraphBuilder, FSceneView& InView, const FRenderTargetBindingSlots& RenderTargets, TRDGUniformBufferRef<FSceneTextureUniformParameters> SceneTextures) override;
+	virtual void PostRenderView_RenderThread(FRDGBuilder& GraphBuilder, FSceneView& InView) override {};
+	virtual void SubscribeToPostProcessingPass(EPostProcessingPass PassId, const FSceneView& View, FAfterPassCallbackDelegateArray& InOutPassCallbacks, bool bIsPassEnabled) override {};
+};
